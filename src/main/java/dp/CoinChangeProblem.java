4133b5e4a7977bf5ae4f7177617e4558bdbd413b
akorol <ext-andrii.korol@here.com>
2019-04-18 16:58:34 +0300

    Add dp tasks

--- src/main/java/dp/CoinChangeProblem.java
+++ src/main/java/dp/CoinChangeProblem.java
@@ -1,8 +1,34 @@
 package dp;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class CoinChangeProblem {
+
+
+	static long getWays2(int n, int[] c) {
+		int[][] A = new int[c.length][(int) (n + 1)];
+
+		for (int i = 0; i < c.length; i++) {
+			A[i][0] = 1;
+		}
+		for (int i = 0; i < c.length; i++) {
+			for (int j = 1; j <= n; j++) {
+				int x = (i > 0) ? A[i - 1][j] : 0;
+				int y = (j - c[i] >= 0) ? A[i][j - c[i]] : 0;
+				A[i][j] = x + y;
+			}
+		}
+
+		for (int i = 0; i < c.length; i++) {
+			for (int j = 0; j < n + 1; j++) {
+				System.out.print(A[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+		return A[c.length - 1][n];
+	}
 
 	static long getWays(long n, long[] c) {
 
@@ -12,40 +38,39 @@ public class CoinChangeProblem {
 		//if w >= c[i] T[i, w] = T[i-1, w] + T[i, w-c[i]]
 		// if w< c[i] T[i, w] = T[i-1, w]
 
-
 		//Fill for w = 0 c={0}
 
-		int[][] T = new int[c.length][(int)(n+1)];
+		long[][] T = new long[c.length][(int) (n + 1)];
 
-		for(int i=0;i<n+1;i++){
-			T[0][i] = 1;
+		for (int i = 0; i < c.length; i++) {
+			T[i][0] = 1;
 		}
 
-		for(int i=1; i<c.length; i++) {
-			long currentCoin = c[i];
-			for (int j = 1; j < n+1; j++) {
-				if(j>= currentCoin) {
-					T[i][j] = T[i-1][j] + T[i][(int)(j-currentCoin)];
-				} else {
-					T[i][j] = T[i-1][j];
-				}
+		for (int coinIndex = 0; coinIndex < c.length; coinIndex++) {
+			long currentCoin = c[coinIndex];
+			for (int sum = 1; sum <= n; sum++) {
+
+				long x = coinIndex > 0 ? T[coinIndex - 1][sum] : 0;
+				long y = (sum - currentCoin) >= 0 ? T[coinIndex][(int) (sum - currentCoin)] : 0;
+
+				T[coinIndex][sum] = x+y;
 			}
 		}
 
-		for(int i=0; i<c.length; i++) {
-			for (int j = 0; j < n+1; j++) {
+		for (int i = 0; i < c.length; i++) {
+			for (int j = 0; j < n + 1; j++) {
 				System.out.print(T[i][j] + " ");
 			}
 			System.out.println();
 		}
 
-		return T[c.length-1][(int)(n)];
+		return T[c.length - 1][(int) (n)];
 
 	}
 
 
 	public static void main(String[] args) {
-		System.out.println(getWays(4, new long[]{1,2,3}));
+		System.out.println(getWays(10, new long[]{2, 5, 3, 6}));
 	}
 
 }
