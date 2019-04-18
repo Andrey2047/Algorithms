7107fe6ffe0d9ec523a0c3b6c07fe34578fa7273
akorol <ext-andrii.korol@here.com>
2019-04-18 18:25:52 +0300

    Add dp tasks

--- src/main/java/dp/CoinChangeProblem.java
+++ src/main/java/dp/CoinChangeProblem.java
@@ -1,34 +1,8 @@
 package dp;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class CoinChangeProblem {
-
-
-	static long getWays2(int n, int[] c) {
-		int[][] A = new int[c.length][(int) (n + 1)];
-
-		for (int i = 0; i < c.length; i++) {
-			A[i][0] = 1;
-		}
-		for (int i = 0; i < c.length; i++) {
-			for (int j = 1; j <= n; j++) {
-				int x = (i > 0) ? A[i - 1][j] : 0;
-				int y = (j - c[i] >= 0) ? A[i][j - c[i]] : 0;
-				A[i][j] = x + y;
-			}
-		}
-
-		for (int i = 0; i < c.length; i++) {
-			for (int j = 0; j < n + 1; j++) {
-				System.out.print(A[i][j] + " ");
-			}
-			System.out.println();
-		}
-
-		return A[c.length - 1][n];
-	}
 
 	static long getWays(long n, long[] c) {
 
@@ -57,15 +31,7 @@ public class CoinChangeProblem {
 			}
 		}
 
-		for (int i = 0; i < c.length; i++) {
-			for (int j = 0; j < n + 1; j++) {
-				System.out.print(T[i][j] + " ");
-			}
-			System.out.println();
-		}
-
 		return T[c.length - 1][(int) (n)];
-
 	}
 
 
